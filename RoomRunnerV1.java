147c1fab394b6cfa0e58bee4ae40ada7c9a6e0e1
dkfinch77 <dkfinch77@gmail.com>
2015-09-29 13:38:31 -0400

    Update RoomRunnerV1.java

--- RoomRunnerV1.java
+++ RoomRunnerV1.java
@@ -2,25 +2,33 @@
 public class RoomRunnerV1 {
 
 	
-	public static void main(String [] args){
+	public static void main(String [] args)
+	{
 		World world = setupWorld();
 
 		NewHero hero = NewHero.choose();
 		world.currentRoom = world.room2;
 
-		while(world.currentRoom != null){
+		while(world.currentRoom != null)
+		{
 			System.out.println(hero.name + " is in room " + world.currentRoom);
-			
-			System.out.print("WHICH DIRECTION DO YOU WANT TO GO: ");
+			Dfl.line("\nWhat would you like to do?\n>");
 			String value = Keyboard.readString();
-			Direction direction = world.currentRoom.findDirection(value);
 			
-			if(direction == null){
-				System.out.println("INVALID DIRECTION!!");
-				continue;
+			if (value.split(" ")[0].equals("go"))
+			{
+				Direction direction = world.currentRoom.findDirection(value.split(" ")[1]);
+			
+				if(direction == null){
+					System.out.println("INVALID DIRECTION!!");
+					continue;
+				}
+			
+				world.moveFromRoom(direction);
 			}
 			
-			world.moveFromRoom(direction);
+			else
+				Dfl.out("Invalid.")
 		}
 		
 		System.out.println("Congratulations, you escaped the world! ");
