00202392187f17a16116166432ad3b9152afbacc
dkfinch77 <dkfinch77@gmail.com>
2015-09-30 21:44:16 -0400

    Create WorldGenerator.java

---
+++ WorldGenerator.java
@@ -0,0 +1,12 @@
+public class WorldGenerator
+{
+  public static World generateNew(int numRooms)
+  {
+    //Use recursion?? Trees? Should go down "branches" using chances until it ends, then finish previuos branches
+  }
+  
+  public static Room generateBranch()
+  {
+  
+  }
+}
