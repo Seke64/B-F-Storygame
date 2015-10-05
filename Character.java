c22ed4fde7db53d813e6e8c9df47cc580a70d983
dkfinch77 <dkfinch77@gmail.com>
2015-10-05 13:01:17 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,8 +1,8 @@
 public class Charcter extends Entity
 {
-  private int[] baseStats;
+  protected int[] baseStats;
   
-  protected static final String[] baseStatsNames = {"Max HP", "Attack", "Defense", "Speed", "Luck"};
+  public static final String[] baseStatsNames = {"Max HP", "Attack", "Defense", "Speed", "Luck"};
   public static final MHP = 0, ATT = 1, DEF = 2, SPD = 3, LUC = 4;
   
   public Charcter(String _name, int[] _baseStats)
