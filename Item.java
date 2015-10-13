3883a8e7db81488e638b05e8c99de035c2f6caaf
dkfinch77 <dkfinch77@gmail.com>
2015-10-13 12:41:40 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,12 +1,10 @@
 public class Item extends Entity
 {
   public String desc;
-  public int[] alterStats;
 
   public Item (String _name, String _desc, int[] _alterStats)
   {
     super(_name);
     desc = _desc;
-    alterStats = _alterStats;
   }
 }
