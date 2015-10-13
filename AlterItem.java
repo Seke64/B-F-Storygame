f50f1108702ebc893d8204bbaf01dcc4b08da789
dkfinch77 <dkfinch77@gmail.com>
2015-10-13 12:51:40 -0400

    Update AlterItem.java

--- AlterItem.java
+++ AlterItem.java
@@ -2,9 +2,9 @@ public class AlterItem extends Item
 {
   public int[] alterStats;
 
-  public AlterItem(_name, _desc, _alterStats)
+  public AlterItem(_name, _desc, _maxCount, _alterStats)
   {
-    super(_name, _desc)
+    super(_name, _desc, _maxCount);
     alterStats = _alterStats;
   }
 }
