48ff1c029b63c79dee3eede24e9b7d72bbe3de6f
dkfinch77 <dkfinch77@gmail.com>
2015-10-13 12:50:26 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,10 +1,12 @@
 public class Item extends Entity
 {
   public String desc;
+  public int maxCount;
 
-  public Item (String _name, String _desc, int[] _alterStats)
+  public Item (String _name, String _desc, int _maxCount)
   {
     super(_name);
     desc = _desc;
+    maxCount = _maxCount;
   }
 }
