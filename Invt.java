b9240c5f797a8c5649315243f4601ed7496b3369
dkfinch77 <dkfinch77@gmail.com>
2015-10-15 13:44:07 -0400

    Update Invt.java

--- Invt.java
+++ Invt.java
@@ -4,13 +4,25 @@ public class Invt
 {
   private ArrayList<ItemStack> items = new ArrayList<ItemStack>();
   
-  public boolean addItem(Item add)
+  public boolean addItem(Item add, int num)
   {
     for (ItemStack i: items)
     {
-      if (add.devName.equals(i.itemStack.devName) && i.add(add))
+      if (add.devName.equals(i.itemStack.devName) && i.whatIsLeftOver(num) <= 0)
       {
-        i.add(add);
+        i.add(num);
+      }
+      
+      else if (add.devName.equals(i.itemStack.devName) && i.whatIsLeftOver(num) > 0)
+      {
+        int left = i.whatIsLeftOver(num);
+        
+        while (left > 0)
+        {
+          items.add(new ItemStack(add, 0);
+          
+          left =- i.itemStack.maxCount;
+        }
       }
     }
   }
