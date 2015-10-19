4a7fb05ca3bbe75fe3458ffe0d8dade2c978994a
dkfinch77 <dkfinch77@gmail.com>
2015-10-19 13:42:21 -0400

    Update Invt.java

--- Invt.java
+++ Invt.java
@@ -2,28 +2,41 @@ import java.util.*;
 
 public class Invt
 {
-  private ArrayList<ItemStack> items = new ArrayList<ItemStack>();
-  
-  public boolean addItem(Item add, int num)
+  private ArrayList<ItemStack> items;
+
+  public Invt()
+  {
+	  items = new ArrayList<ItemStack>();
+  }
+
+  public void addItem(Item add, int num)
   {
     for (ItemStack i: items)
     {
-      if (add.devName.equals(i.itemStack.devName) && i.whatIsLeftOver(num) <= 0)
+      if (add.devName.equals(i.itemStack.devName))
       {
-        i.add(num);
-        
-        return true;
+		if (i.whatIsLeftOver(num) <= 0)
+		{
+          i.add(num);
+          num -= num;
+	    }
+	    else
+	    {
+	      int addNow = (num - i.whatIsLeftOver(num));
+		  i.add(addNow);
+		  num -= addNow;
+		}
       }
     }
-    
+
     while (num > 0)
     {
-      if (num > add.maxCount
+      if (num > add.maxCount)
       {
-        items.add(new ItemStack(add, add.maxCount);
+        items.add(new ItemStack(add, add.maxCount));
         num -= add.maxCount;
       }
-      
+
       else
       {
         items.add(new ItemStack(add, num));
@@ -31,16 +44,16 @@ public class Invt
       }
     }
   }
-  
+
   public String toString()
   {
     String out = "Inventory:";
-    
+
     for (ItemStack is: items)
     {
       out += "\n" + is;
     }
-    
+
     return out;
   }
 }
