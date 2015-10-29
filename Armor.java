07e961b6df006d8d553dec408872705c4c5a8ea9
dkfinch77 <dkfinch77@gmail.com>
2015-10-29 13:14:54 -0400

    Create Armor.java

---
+++ Armor.java
@@ -0,0 +1,7 @@
+public class Armor extends AlterItem
+{
+  public Armor(String _name, String _devName, String _desc, int _maxCount, int[] _alterStats)
+  {
+    super(_name, _devName, _desc, _maxCount, _alterStats);
+  }
+}
